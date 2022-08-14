cbc832c69cd6427668468f53f53a04678c45d3bb
Lukas Musilek <lukas.musilek@gmail.com>
2022-08-14 18:07:41 +0100

    added scope for expenses and income sums for the user and updated balance card

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :transactions
+  
+  def expenses
+    # self.transactions.where(:categories => { :category_type => "expense" })
+    self.transactions.where(category: Category.where(category_type: "expense")).sum(:amount)
+  end
+
+  def income
+    self.transactions.where(category: Category.where(category_type: "income")).sum(:amount)
+  end
 end
