0f70cea2d1567beec69f1139ba739e381128a7ce
Lukas Musilek <lukas.musilek@gmail.com>
2022-08-16 21:35:05 +0100

    added extra style to from, more to go

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -13,14 +13,14 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
+  config.wrappers :default, class: [:input, :form__group],
     hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
     # You can make any of these extensions optional by
     # renaming `b.use` to `b.optional`.
-
+    b.use :input, class: "form__input"
     # Determines whether to use HTML5 (:email, :url, ...)
     # and required attributes
     b.use :html5
