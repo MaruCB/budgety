ca9cd800db219d17af9fe352f076f4bae063371e
Lukas Musilek <lukas.musilek@gmail.com>
2022-08-19 12:14:37 +0100

    clean up

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -4,13 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["date", "notes", "category", "amount"]
   connect() {
-    // console.log(this.dateTarget)
-    // console.log(this.notesTarget)
-    // console.log(this.categoryTarget)
-    // console.log(this.amountTarget)
-    
-    // console.log(this.categoryTarget.firstChild.classList.add("gray-placeholder"))
-    // console.log(this.amountTarget.style.color = 'blue')
   }
 }
 
